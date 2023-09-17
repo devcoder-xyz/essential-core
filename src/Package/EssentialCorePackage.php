f67f7f8c365d6f4a6a63e77e649e27ee84574ff8
F.Michel <fadymichel@protonmail.com>
2023-09-17 16:01:04 +0000

    Added new unit tests to the project.

--- src/Package/EssentialCorePackage.php
+++ src/Package/EssentialCorePackage.php
@@ -6,7 +6,7 @@ use DevCoder\Listener\EventDispatcher;
 use DevCoder\Listener\ListenerProvider;
 use DevCoder\Renderer\PhpRenderer;
 use DevCoder\Route;
-use DevCoder\Router;
+use DevCoder\Router as DevCoderRouter;
 use Essential\Core\Command\CacheClearCommand;
 use Essential\Core\Command\DebugContainerCommand;
 use Essential\Core\Command\DebugEnvCommand;
@@ -15,7 +15,6 @@ use Essential\Core\Command\MakeCommandCommand;
 use Essential\Core\Command\MakeControllerCommand;
 use Essential\Core\ErrorHandler\ErrorRenderer\HtmlErrorRenderer;
 use Essential\Core\ErrorHandler\ExceptionHandler;
-use Essential\Core\Manager\CacheManager;
 use Essential\Core\Middlewares\RouterMiddleware;
 use Essential\Core\Router\Bridge\RouteFactory;
 use LogicException;
@@ -30,42 +29,28 @@ final class EssentialCorePackage implements PackageInterface
 {
     public function getDefinitions(): array
     {
-        $services = [];
-        if (class_exists(EventDispatcher::class)) {
-            $services[EventDispatcherInterface::class] = static function (ContainerInterface $container): ?EventDispatcherInterface {
-                $events = $container->get('essential.listeners');
-                $provider = new ListenerProvider();
-                foreach ($events as $event => $listeners) {
-                    if (is_array($listeners)) {
-                        foreach ($listeners as $listener) {
-                            $provider->addListener($event, $container->get($listener));
-                        }
-                    } elseif (is_object($listeners)) {
-                        $provider->addListener($event, $listeners);
-                    } else {
-                        $provider->addListener($event, $container->get($listeners));
-                    }
-                }
-                return new EventDispatcher($provider);
-            };
-        }
-
-        if (class_exists(Router::class)) {
-            $services['router'] = static function (ContainerInterface $container): object {
-                /**
-                 * @var array<\Essential\Core\Router\Route> $routes
-                 */
-                $routes = $container->get('essential.routes');
-                $factory = new RouteFactory();
-
-                $router = new Router([], $container->get('app.url'));
-                foreach ($routes as $route) {
-                    $router->add($factory->createDevCoderRoute($route));
-                }
-                return $router;
-            };
-        }
         return [
+                EventDispatcherInterface::class => static function (ContainerInterface $container): ?EventDispatcherInterface {
+
+                    if (!class_exists(EventDispatcher::class)) {
+                        throw new LogicException('The "EventDispatcherInterface" requires the presence of an event dispatcher. You can install it by running "composer require devcoder-xyz/php-event-dispatcher".');
+                    }
+
+                    $events = $container->get('essential.listeners');
+                    $provider = new ListenerProvider();
+                    foreach ($events as $event => $listeners) {
+                        if (is_array($listeners)) {
+                            foreach ($listeners as $listener) {
+                                $provider->addListener($event, $container->get($listener));
+                            }
+                        } elseif (is_object($listeners)) {
+                            $provider->addListener($event, $listeners);
+                        } else {
+                            $provider->addListener($event, $container->get($listeners));
+                        }
+                    }
+                    return new EventDispatcher($provider);
+                },
                 Application::class => static function (ContainerInterface $container): Application {
                     $commandList = $container->get('essential.commands');
                     $commands = [];
@@ -75,9 +60,6 @@ final class EssentialCorePackage implements PackageInterface
                     $application = new Application();
                     $application->addCommands($commands);
                     return $application;
-                },
-                CacheManager::class => static function (ContainerInterface $container) {
-                    return new CacheManager($container->get('essential.cache_dir'));
                 },
                 'render' => static function (ContainerInterface $container) {
                     if (class_exists(Environment::class)) {
@@ -91,6 +73,25 @@ final class EssentialCorePackage implements PackageInterface
                     }
 
                     throw new LogicException('The "render" requires a Renderer to be available. You can choose between installing "devcoder-xyz/php-renderer" or "twig/twig" depending on your preference.');
+                },
+
+                'router' => static function (ContainerInterface $container): object {
+
+                    if (!class_exists(DevCoderRouter::class)) {
+                        throw new LogicException('The Router component requires the presence of a router library. You can install it by running "composer require devcoder-xyz/php-router".');
+                    }
+
+                    /**
+                     * @var array<\Essential\Core\Router\Route> $routes
+                     */
+                    $routes = $container->get('essential.routes');
+                    $factory = new RouteFactory();
+
+                    $router = new Router([], $container->get('app.url'));
+                    foreach ($routes as $route) {
+                        $router->add($factory->createDevCoderRoute($route));
+                    }
+                    return $router;
                 },
                 RouterMiddleware::class => static function (ContainerInterface $container) {
                     return new RouterMiddleware($container->get('router'), response_factory());
@@ -106,7 +107,7 @@ final class EssentialCorePackage implements PackageInterface
                         ]
                     );
                 }
-            ] + $services;
+            ];
     }
 
     public function getParameters(): array
